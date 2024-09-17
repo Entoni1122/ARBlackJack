2730010b52a1374d4a40f33bc0e56eb95ebe3612
Entoni1122 <entonishkalla34@gmail.com>
2024-09-17 14:27:43 +0200

    Ui for Winner

--- Assets/_Scripts/Managers/GameManagers.cs
+++ Assets/_Scripts/Managers/GameManagers.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
+using TMPro;
 using UnityEngine;
 
 public class GameManagers : MonoBehaviour
@@ -13,6 +13,12 @@ public class GameManagers : MonoBehaviour
 
     public static Action OnRestartGameCallBack;
     public static Action OnDealerFreeWillCallBack;
+
+    [SerializeField] List<GameObject> winners;
+    [SerializeField] List<GameObject> playerTracked;
+    [SerializeField] GameObject winnerTextContainer;
+    TextMeshProUGUI winnerText;
+    int maxTableValue = 0;
 
     int playerDoneCount;
 
@@ -28,16 +34,14 @@ public class GameManagers : MonoBehaviour
         BlackJackPlayer.OnPLayerTurnEndCallBack -= PlayerFinished;
         BlackJackDealer.OnCalculatePoints -= CalculateWinner;
     }
-    [SerializeField] List<GameObject> winners;
-    [SerializeField] List<GameObject> playerTracked;
-    int maxTableValue = 0;
+
     public void OnPlayerEnd()
     {
+        //Checks who are the player still in game removing who is busted
         for (int i = playerInTheGame.Count - 1; i >= 0; i--)
         {
             BlackJackPlayer playerComponent = playerInTheGame[i].GetComponentInChildren<BlackJackPlayer>();
             int playerPoints = playerComponent.Points;
-
 
             if (playerInTheGame[i].GetComponentInChildren<BlackJackPlayer>().CurrentState == State.Stand)
             {
@@ -50,13 +54,15 @@ public class GameManagers : MonoBehaviour
         }
         winners = playerTracked;
     }
+
     private void Start()
     {
+        winnerText = winnerTextContainer.GetComponentInChildren<TextMeshProUGUI>();
         playerDoneCount = playerInTheGame.Count;
     }
-    //Checks when player are fully finished (bust, stand) so that the dealer can play second hand 
     public void PlayerFinished()
     {
+        //Checks when player are fully finished (bust, stand) so that the dealer can play second hand 
         playerDoneCount--;
         if (playerDoneCount <= 0)
         {
@@ -84,61 +90,77 @@ public class GameManagers : MonoBehaviour
         {
             case GameState.Players:
                 break;
-
             case GameState.Dealer:
                 OnPlayerEnd();
                 OnDealerFreeWillCallBack?.Invoke();
                 break;
-
             default:
                 break;
         }
     }
+
+    public void ShowWinnerTEXT(string winner)
+    {
+        winnerText.text += " " + winner;
+    }
     public void CalculateWinner(int points)
     {
+        print("Caluclate winners");
         //If the dealer got more then 21 calculate the winner form the player that are in stand
         //Otherwise calculate each time the dealer picks a card, who need to go out
         if (points > 21)
         {
-            print("dealer lose");
-            if (playerTracked.Count > 0)
+            if (playerTracked.Count > 0 && playerTracked.Count < 4)
             {
                 for (int i = 0; i < playerTracked.Count; i++)
                 {
-                    print(playerTracked[i].GetComponentInChildren<BlackJackPlayer>().Names);
+                    //Put winner at the start of the sentence
+                    winnerText.text += "Winners : ";
+                    ShowWinnerTEXT(playerTracked[i].GetComponentInChildren<BlackJackPlayer>().Names);
                 }
             }
+            else
+            {
+                ShowWinnerTEXT("Dealer Busted");
+            }
+            winnerTextContainer.gameObject.SetActive(true);
             return;
         }
         if (points == maxTableValue)
         {
-            print("TIE");
+            winnerTextContainer.gameObject.SetActive(true);
+            ShowWinnerTEXT("TIE");
         }
         if (points > maxTableValue)
         {
             if (playerTracked.Count > 0)
             {
-                for (int i = 0; i < playerTracked.Count; i++)
-                {
-                    print(playerTracked[i].GetComponentInChildren<BlackJackPlayer>().Names + " Lost");
-                }
+                //for (int i = 0; i < playerTracked.Count; i++)
+                //{
+                //    print(playerTracked[i].GetComponentInChildren<BlackJackPlayer>().Names + " Lost");
+                //}
+                winnerTextContainer.gameObject.SetActive(true);
+                ShowWinnerTEXT("Dealer Won");
             }
             return;
         }
-
-        foreach (GameObject player in winners)
+        //Checks the remaining player to bust who is under the dealer
+        for (int i = 0; i < winners.Count; i++)
         {
-            BlackJackPlayer playerComponent = player.GetComponentInChildren<BlackJackPlayer>();
+            BlackJackPlayer playerComponent = winners[i].GetComponentInChildren<BlackJackPlayer>();
             int playerPoints = playerComponent.Points;
 
             if (playerPoints < points || maxTableValue > playerPoints)
             {
                 playerComponent.ChangeState(State.Bust);
 
-                winners.Remove(player.gameObject);
+                winners.Remove(winners[i].gameObject);
+
+                //Check if the last player was removed so that the dealer wins without doing another time the method
                 if (winners.Count <= 0)
                 {
-                    print("Dealer Win");
+                    winnerTextContainer.gameObject.SetActive(true);
+                    ShowWinnerTEXT("Dealer Won");
                 }
             }
         }
